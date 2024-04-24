c23ea9b3810cb2f1bd867ffe0eecafdf30d173fa
Amit Singh <106347663+i-amitsingh@users.noreply.github.com>
2024-04-24 09:51:51 +0530

    Add files via upload

---
+++ lab/us/ultrasonic.py
@@ -0,0 +1,36 @@
+import time
+import RPi.GPIO as GPIO
+
+p=0
+
+while p<20:
+      GPIO.setmode(GPIO.BOARD)
+
+      PIN_TRIGGER = 7
+      PIN_ECHO = 11
+
+      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+      GPIO.setup(PIN_ECHO, GPIO.IN)
+
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+
+      time.sleep(0.00001)
+
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+      while GPIO.input(PIN_ECHO)==0:
+            pulse_start_time = time.time()
+      while GPIO.input(PIN_ECHO)==1:
+            pulse_end_time = time.time()
+
+      pulse_duration = pulse_end_time - pulse_start_time
+      distance = round(pulse_duration * 17150, 2)
+      print ("Distance:",distance,"cm")
+      p=p+1
+      
+      
+      time.sleep(2)
+         
+GPIO.cleanup()
